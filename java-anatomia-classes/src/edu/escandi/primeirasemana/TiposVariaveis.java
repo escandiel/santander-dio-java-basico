1f7ef5748522237af1646eed3539ce57e5a66056
Victor Escandiel <joaoescandielb@gmail.com>
2023-09-28 14:03:15 -0300

    AboutMe Class

---
+++ java-anatomia-classes/src/edu/escandi/primeirasemana/TiposVariaveis.java
@@ -0,0 +1,13 @@
+package edu.escandi.primeirasemana;
+
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+
+        //TIPOS PRIMITIVOS
+        // double salarioMinimo = 2500.33;
+        // short numeroCurto = 1;
+        // int numeroNormal = numeroCurto;
+        // short numeroCurto2 = (short) numeroNormal;
+        // final double VALOR_DE_PI = 3.14;
+    }
+}
